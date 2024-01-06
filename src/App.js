a83005741961c7c042ff48bcf4320bb65306fefb
Gavriil1 G <airbnbguide@gmail.com>
2024-01-06 12:15:13 +0000

    from here i start working independantly. Created test login page added username password remember me login and dont have account fields

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Route, Switch } from "react-router-dom";
 import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
+import SignInFormtest from "./pages/auth/SignInFormtest";
 import PostCreateForm from "./pages/posts/PostCreateForm";
 import PostPage from "./pages/posts/PostPage";
 import PostsPage from "./pages/posts/PostsPage";
@@ -53,6 +54,7 @@ function App() {
             )}
           />
           <Route exact path="/signin" render={() => <SignInForm />} />
+          <Route exact path="/signintest" render={() => <SignInFormtest />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route exact path="/posts/create" render={() => <PostCreateForm />} />
           <Route exact path="/posts/:id" render={() => <PostPage />} />
