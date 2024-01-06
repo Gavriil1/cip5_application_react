3c80c0569d36e734b5a9dc8b8676144e1a4ea84f
Gavriil1 G <airbnbguide@gmail.com>
2024-01-06 09:58:50 +0000

    make some changes according to (Text instructions: A few last things) documentation from the student material

---
+++ src/components/NotFound.js
@@ -0,0 +1,12 @@
+import logo from "../assets/logo.png";
+import styles from "../styles/NotFound.module.css";
+
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div>Sorry, the page you are looking for doesjnt exist </div>
+  )
+}
+
+export default NotFound
